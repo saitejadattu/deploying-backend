daca3044117283c05cc46a39ddd127d4f5883638
saitejadattu <164032855+saitejadattu@users.noreply.github.com>
2024-11-14 16:34:13 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const Router = express.Router()
 const { User } = require("./models")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 const Authorization = async (req, res, next) => {
